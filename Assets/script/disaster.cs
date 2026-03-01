5f7252e857b5b6ad4a38e5f3b6010d16191df46d
peanut-sandvich <leoisapeanut@gmail.com>
2026-03-01 13:49:17 +0800

    disaster triggering

--- Assets/script/disaster.cs
+++ Assets/script/disaster.cs
@@ -2,19 +2,68 @@ using UnityEngine;
 
 public class disaster : MonoBehaviour
 {
-    public float Oxygen;
-    public float Nitrogen;
-    public float Temp;
+    public float oxygen;
+    public float pressure;
+    public float temperature;
+    public float hullIntegrity;
+    public bool depressValve;
+    
+    public float disasteCheckInterval;
+    public float disasterChance;
+    public float disasterTimer;
+    public float baseDepressRate;
+
+    public bool disasterTrigger;
+
+    public bool ifBreathableAtmo = true;
+    public bool ifLethalAtmo = false;
+    public float breathableAtmoLimit;
+    public float lethalAtmoLimit;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        oxygen = 0.21f; //in percentage 0-1
+        pressure = 1013.25f; //in hectopascals(hPa)
+        temperature = 25f; //in degrees Celsius
+        hullIntegrity = 1f; //in percentage 0-1 
+        depressValve = false;
+
+        //edit below to tweak difficulty
+        disasterChance = 0.3f;// 0 to 1
+        disasterCheckInterval = 15f;//in seconds
+        baseDepressRate = 5f;//In hPa lost per second
+
+        breathableAtmoLimit = 200f;//hPa
+        lethalAtmoLimit = 50f;//hPa
+
+
+
+
+
+        disasterTimer = disasteCheckInterval;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (disasterTimer > 0 ) {
+            disasterTimer -= Time.deltaTime;
+        }
+        if (disasterTimer <= 0) {
+            if (float random.Next(0,1) <= disasterChance) {
+                disasterTrigger = true;
+            }
+            disasterTimer = disasterCheckInterval;
+        }
+
+        if (disasterTrigger = true) {
+
+        }
+
+        pressure -= (baseDepressRate * Time.deltaTime)
+
     }
 }
+ 
