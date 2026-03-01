b284f37fc97dd587b73a1aa6379d33a8b9149811
DevEclipse2 <151531603+DevEclipse2@users.noreply.github.com>
2026-03-01 15:38:18 +0800

    tutorial

--- Assets/script/Tutorial.cs
+++ Assets/script/Tutorial.cs
@@ -3,18 +3,41 @@ using UnityEngine;
 public class Tutorial : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    AudioSource[] audioSources;
-    float[] Lengths;
+    public AudioSource[] audioSources;
+    public float[] Lengths;
     public bool[] auto;
-    public int index;
+    public int index = 0;
+    float timer;
+    bool playing;
     void Start()
     {
-        
+        audioSources[index].Play();
+        playing = true;
     }
+    public void Progress()
+    {
 
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if(playing)
+        {
+            timer += Time.deltaTime;
+        }
+        if (timer > Lengths[index] && playing)
+        {
+
+            audioSources[index].Stop();
+            index++;
+            timer = 0;
+            playing = false;
+            if (auto[index])
+            {
+                playing = true;
+                audioSources[index].Play();
+
+            }
+        }
     }
 }
