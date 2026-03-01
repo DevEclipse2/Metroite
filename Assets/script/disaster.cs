b672f85f21f5119e9bb75293ab884189fe798def
peanut-sandvich <leoisapeanut@gmail.com>
2026-03-01 14:37:18 +0800

    disasters.cs

--- Assets/script/disaster.cs
+++ Assets/script/disaster.cs
@@ -8,17 +8,31 @@ public class disaster : MonoBehaviour
     public float hullIntegrity;
     public bool depressValve;
     
-    public float disasteCheckInterval;
+    public float disasterCheckInterval;
     public float disasterChance;
     public float disasterTimer;
     public float baseDepressRate;
+    public float baseOxyConsumRate;
+    public float damageDepressMultiplier;
 
     public bool disasterTrigger;
 
+    //say check for these values to display on screens
     public bool ifBreathableAtmo = true;
     public bool ifLethalAtmo = false;
-    public float breathableAtmoLimit;
-    public float lethalAtmoLimit;
+    public float breathablePressLimit;
+    public float lethalPressLimit;
+    public float breathableOxyLimit;
+
+    public float scbaRemaining;
+    public float scbaUseTime;
+    public float scbaRefillRate;
+    public bool scbaInUse;
+
+    //check for these to trigger game-overs
+    public bool gameOverOxy;
+    public bool gameOverDepress;
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -35,34 +49,71 @@ public class disaster : MonoBehaviour
         disasterCheckInterval = 15f;//in seconds
         baseDepressRate = 5f;//In hPa lost per second
 
-        breathableAtmoLimit = 200f;//hPa
-        lethalAtmoLimit = 50f;//hPa
+        breathablePressLimit = 200f;//hPa
+        lethalPressLimit = 50f;//hPa
+        breathableOxyLimit = 0.05f;//percentage 0-1
+
+        scbaUseTime = 60f;//seconds
+        scbaRefillRate = 2f;//ratio against use rate 
 
 
 
 
 
-        disasterTimer = disasteCheckInterval;
+        disasterTimer = disasterCheckInterval;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //base depress and O2 consum.
+        pressure -= (baseDepressRate * Time.deltaTime);
+        oxygen -= (baseOxyConsumRate * Time.deltaTime);
+
+        //damage-caused depress.
+        pressure -= (damageDepressMultiplier * (hullIntegrity - 1) * Time.deltaTime);
+
+        
         if (disasterTimer > 0 ) {
             disasterTimer -= Time.deltaTime;
         }
         if (disasterTimer <= 0) {
-            if (float random.Next(0,1) <= disasterChance) {
+            float disasterRand = Random.Range(0, 1);
+            if (disasterRand <= disasterChance) {
                 disasterTrigger = true;
             }
             disasterTimer = disasterCheckInterval;
         }
 
         if (disasterTrigger = true) {
-
+            //call for a random disaster to happen here
         }
 
-        pressure -= (baseDepressRate * Time.deltaTime)
+        pressure -= (baseDepressRate * Time.deltaTime);
+
+        
+        //checks for atmosphere survivability
+        if (pressure <= lethalPressLimit) {
+            gameOverDepress = true;
+        }
+        else if ((oxygen <= breathableOxyLimit) || (pressure <= breathablePressLimit)) {
+            ifBreathableAtmo = false;
+        }
+        else {
+            ifBreathableAtmo = true;
+        }
+
+        //SCBA (Self-contained Breathing Apperatus) (basically emergency air tank) behaviour
+        if (scbaRemaining <= 0) {
+            gameOverOxy = true;
+        }
+        if (ifBreathableAtmo == true) {
+            scbaInUse = true;
+            scbaRemaining -= (Time.deltaTime / scbaUseTime);
+        }
+        else {
+            scbaRemaining += (scbaRefillRate * (Time.deltaTime * scbaRefillRate));
+        }
 
     }
 }
