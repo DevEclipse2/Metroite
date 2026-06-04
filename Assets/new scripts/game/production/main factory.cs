63ee67a9ea2699c49658126e192d8e0d5bf8461f
DevEclipse2 <151531603+DevEclipse2@users.noreply.github.com>
2026-06-04 23:10:02 +0800

    new factory system
    optimised for efficiency

---
+++ Assets/new scripts/game/production/main factory.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class mainfactory : MonoBehaviour
+{
+
+
+    struct excavatorNode
+    {
+        bool powered;
+        float consumptionRate;
+        int[] outputs;
+        float[] productionRate;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/new scripts/game/production/resources.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class resources : MonoBehaviour
+{
+    //ok so resources are either raw material, industrial material or energy
+
+    public int EnergyElectricity        = -1;
+    public int RawWaterIce              = 0;
+    public int RawGasHydrogen           = 1;
+    public int RawGasIsotopeDeuterium   = 2;
+    public int RawGasOxygen             = 3;
+    public int RawGasArgon              = 4;
+    public int RawGasMethane            = 5;
+    public int RawImpureLithiumOre      = 6;
+    public int RawImpureIronOre         = 7;
+    public int RawImpureCopperOre       = 8;
+    public int RawImpureZincOre         = 9;
+
+}
