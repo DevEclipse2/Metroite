e0883a526cf27bcf38659939786352ae85f1486d
DevEclipse2 <151531603+DevEclipse2@users.noreply.github.com>
2026-06-14 18:19:28 +0800

    localGravity

---
+++ Assets/new scripts/game/physics/gravityField.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class gravityField : MonoBehaviour
+{
+
+    public GameObject[] gravMarkers;
+    public MeshCollider Rooms;
+    public bool enabled;
+    void generateField()
+    {
+
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/new scripts/game/physics/gravityMarker.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class gravityMarker : MonoBehaviour
+{
+    public Vector3 direction;
+    public float acceleration;
+    public float distance;
+    public bool uselinear;
+    public bool usecubic;
+    public bool steepdropoff;
+
+}
