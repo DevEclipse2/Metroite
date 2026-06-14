e9fb21d7a2376d02532c24e80b68d87caaa90008
DevEclipse2 <151531603+DevEclipse2@users.noreply.github.com>
2026-06-14 23:35:06 +0800

    set objects within to be active

--- Assets/new scripts/game/physics/Bulkheads.cs
+++ Assets/new scripts/game/physics/Bulkheads.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,9 @@ public class Bulkheads : MonoBehaviour
     public GameObject[] connections;
     public List<int> connectionIndexes;
     public HashSet<GameObject> ObjectsPresent;
+    public byte playerCount = 0;
+    public byte vehicleCount = 0;
+    private bool active;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,7 +22,59 @@ public class Bulkheads : MonoBehaviour
     }
     public void setGravFieldActive(bool value)
     {
+        active = value;
         gravityFields.GetComponent<gravityField>().enabled = value;
+        if (value)
+        {
+            foreach (GameObject presentobjects in ObjectsPresent)
+            {
+                try
+                {
+                    if (presentobjects.CompareTag("AlwaysOn"))
+                    {
+                        presentobjects.GetComponent<Collider>().enabled = true;
+                    
+                    }
+                    else
+                    {
+                        presentobjects.GetComponent<Collider>().enabled = true;
+                        presentobjects.SetActive(true);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.Log(e.Message);
+                }
+            }
+        }
+        else
+        {
+            foreach (GameObject presentobjects in ObjectsPresent)
+            {
+                try
+                {
+                    if (presentobjects.CompareTag("AlwaysOn"))
+                    {
+                    
+                        presentobjects.GetComponent<Rigidbody>().Sleep();
+                        presentobjects.GetComponent<Collider>().enabled = false;
+                    
+                    }
+                    else
+                    {
+                    
+                        presentobjects.GetComponent<Rigidbody>().Sleep();
+                        presentobjects.GetComponent<Collider>().enabled = false;
+                        presentobjects.SetActive(false);
+                    
+                    }
+                }
+                catch(Exception e)
+                {
+                    Debug.Log(e.Message);
+                }
+            }
+        }
     }
     public void Unload()
     {
@@ -32,6 +88,7 @@ public class Bulkheads : MonoBehaviour
         yield return new WaitForSeconds(4f);
         setGravFieldActive(false);
         Debug.Log("unloaded");
+       
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -41,7 +98,26 @@ public class Bulkheads : MonoBehaviour
         // Notify Manager if the object is the player
         if (other.CompareTag("Player"))
         {
+            playerCount++;
             SceneLoader.playerMoved(id, connectionIndexes);
+        }
+        else if (other.CompareTag("Vehicle"))
+        {
+            vehicleCount++;
+            SceneLoader.vehicleMoved(id, connectionIndexes);
+        }
+        if (active)
+        {
+            if (other.CompareTag("AlwaysOn"))
+            {
+                other.enabled = true;
+
+            }
+            else
+            {
+                other.enabled = true;
+                other.gameObject.SetActive(true);
+            }
         }
     }
 
@@ -50,12 +126,15 @@ public class Bulkheads : MonoBehaviour
         ObjectsPresent.Remove(other.gameObject);
         if (other.CompareTag("Player"))
         {
+            playerCount--;
             SceneLoader.playerMoved(id, connectionIndexes);
+        }
+        else if(other.CompareTag("Vehicle"))
+        {
+            vehicleCount--;
+            SceneLoader.vehicleMoved(id, connectionIndexes);
+
         }
     }
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/new scripts/game/physics/SceneLoader.cs
+++ Assets/new scripts/game/physics/SceneLoader.cs
@@ -7,18 +7,41 @@ public static class SceneLoader
 {
     public static List<GameObject> bulkheads;
     public static List<int>     ActiveIndexes;
+    public static List<int>     OccupiedIndexes;// occupied indexes are areas where a player is doing remote controlled shi in
     public static void playerMoved(int selfId, List<int> connectedIndexes)
     {
+
         foreach (int index in ActiveIndexes) {
             if (!connectedIndexes.Contains(index))
             {
-                bulkheads[index].GetComponent<Bulkheads>().Unload();
+                if (bulkheads[index].GetComponent<Bulkheads>().playerCount == 0 && bulkheads[index].GetComponent<Bulkheads>().vehicleCount == 0)
+                {
+                    bulkheads[index].GetComponent<Bulkheads>().Unload();
+                }
             }
         }
+        bulkheads[selfId].GetComponent<Bulkheads>().setGravFieldActive(true);
         foreach (int index in connectedIndexes)
         {
             bulkheads[index].GetComponent<Bulkheads>().setGravFieldActive(true);
         }
+        //when a player moves into a bulkhead, adjacent rooms are also loaded, while non adjacent rooms that were loaded before unload after 3 seconds
+    }
+    public static void vehicleMoved(int selfId, List<int> connectedIndexes)
+    {
+
+        foreach (int index in ActiveIndexes)
+        {
+            if (!connectedIndexes.Contains(index))
+            {
+                if (bulkheads[index].GetComponent<Bulkheads>().playerCount == 0 && bulkheads[index].GetComponent<Bulkheads>().vehicleCount == 0)
+                {
+                    bulkheads[index].GetComponent<Bulkheads>().Unload();
+                }
+            }
+        }
+        bulkheads[selfId].GetComponent<Bulkheads>().setGravFieldActive(true);
+
         //when a player moves into a bulkhead, adjacent rooms are also loaded, while non adjacent rooms that were loaded before unload after 3 seconds
     }
     public static int Init(GameObject gameobj,GameObject[] connections)
